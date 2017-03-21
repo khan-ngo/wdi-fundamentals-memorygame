24aa56c0189f1a2c41cae3102f601f6eefe2144b
kqngo <khan.ngo@gmail.com>
2017-03-20 23:05:59 -0400

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,39 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
+
+var cardId = "0";
+
+cardsInPlay.push(cards[cardId]);
+
+//create a function to store all steps that should happen when the user flips a card.
+var flipCard = function(cardID) {
+	// Display card that was just flipped
+	console.log("User flipped " + cards[cardId]);
+		// Add logic to check to see if the length of the CardsInPlay array is 2
+		// also check equality between the two cards match
+		if (cardsInPlay.length === 2) {
+			if (cardsInPlay[0] === cardsInPlay[1]) {
+				alert("You found a match!");
+			} else {
+				alert("Sorry, try again!");
+			}
+		}
+};
 
 
-// Add to Array
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+cardsInPlay.push(cards[cardId]);
 
-// Add logic to check to see if the length of the CardsInPlay array is 2
-// also check equality between the two cards match
-if (cardsInPlay.length ===2) {
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
+flipCard(0);
+flipCard(2);
+
+// create a function to store the steps to check for a match.
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("you've found a match!");
 	} else {
-		alert("Sorry, try again!");
+		console.log("Sorry, try again.");
 	}
 };
 
-// Display card that was just flipped
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-
+checkForMatch(flipCard);
 
