4f73e0b05a2eac1eb0a838ee745d94ab264a2406
kqngo <khan.ngo@gmail.com>
2017-03-20 22:04:59 -0400

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
- console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
 
-  console.log("User flipped " + cardOne);
-  console.log("User flipped " + cardThree);
+// Add to Array
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+// Add logic to check to see if the length of the CardsInPlay array is 2
+// also check equality between the two cards match
+if (cardsInPlay.length ===2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again!");
+	}
+};
+
+// Display card that was just flipped
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+
+
